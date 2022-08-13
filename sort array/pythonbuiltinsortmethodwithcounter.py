9d9edf0782fa31d5ed8b56e1fd4024e3082a3cfc
dazyfreez <dazyfreez@gmail.com>
2022-08-13 20:03:14 +0200

    s

--- sort array/pythonbuiltinsortmethodwithcounter.py
+++ sort array/pythonbuiltinsortmethodwithcounter.py
@@ -3,7 +3,7 @@ from array import array
 
 array = [5, 4, 3, 2, 1, 3, 5, 6, 8, 10, 9, 7, 6, 5, 4, 3, 2, 1, 0]
 print("Array before sorting: ", array)
-print( sorted(array))
+print(sorted(array))
 print(len(array))
 print("do you want to count certain nubber in the array?")  
 print("if yes, please enter the number")
