5e058cabc1c764a0b54de652a71c4e77a3ed636f
dazyfreez <dazyfreez@gmail.com>
2022-08-14 17:20:19 +0200

    s

--- sort array/letusercreatearray.py
+++ sort array/letusercreatearray.py
@@ -1,3 +1,17 @@
 print("how many digits should the array have")
 number = int(input())    # number of digits in the array
-
+array = []
+for i in range(number):
+    array.append(int(input()))
+print("Array before sorting: ", array)
+print(sorted(array))
+print("do you want to count certain nubber in the array?")
+print("if yes, please enter the number")
+print("if no, please enter 0")
+number = int(input())
+if number == 0:
+    print("Array after sorting: ", sorted(array))
+else:
+    print("Array after sorting: ", sorted(array))
+    print("the number of", number, "is", array.count(number))
+# array = [5, 4, 3, 2, 1, 3, 5, 6, 8, 10, 9, 7, 6, 5, 4, 3, 2, 1, 0]    # array to be sorted
