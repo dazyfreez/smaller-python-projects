f5cd20b1a67e541785f6c2b7c826a31791c503bf
dazyfreez <dazyfreez@gmail.com>
2022-08-14 17:33:48 +0200

    s

--- sort array/letusercreatearray.py
+++ sort array/letusercreatearray.py
@@ -2,10 +2,10 @@ import numpy as np
 print("how many digits should the array have")
 number = int(input())    # number of digits in the array
 array = np.random.randint(10,90,(4,5)) # random numpy array of shape (4,5)
-print("first array", array)                                                        
-print("Array before sorting: ", array)
-print(sorted(array))
-print("do you want to count certain nubber in the array?")
+print("first array", array) # print the array                                        
+print("Array before sorting: ", array) # print the array before sorting
+print(sorted(array)) # print the array after sorting 
+print("do you want to count certain nubber in the array?") 
 print("if yes, please enter the number")
 print("if no, please enter 0")
 number = int(input())
