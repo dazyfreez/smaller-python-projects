20fa0aba8ea79a1ff8d4aed4b4f7efee98850381
dazyfreez <dazyfreez@gmail.com>
2022-08-23 01:53:42 +0200

    s

--- math/math.py
+++ math/math.py
@@ -4,4 +4,15 @@ import math
 print("Welcome to the calculator")
 
 #create the multible function for the different operations
-def 
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
