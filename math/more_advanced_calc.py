b33efe68a8183bf8b77e2bb069fed9657667e210
dazyfreez <dazyfreez@gmail.com>
2022-08-25 23:52:27 +0200

    s

--- math/more_advanced_calc.py
+++ math/more_advanced_calc.py
@@ -3,16 +3,3 @@
 print("Welcome to the calculator")
 
 #create the multible function for the different operations
-def add(x,y):
-    return x+y
-
-def subtract(x,y):
-    return x-y
-
-def multiply(x,y):
-    return x*y
-
-def divide(x,y):
-    return x/y
-
-print("Select operation.")
