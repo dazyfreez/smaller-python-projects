3df4b3eade2e682a91d05bc68a8ba2d11a40e31f
dazyfreez <dazyfreez@gmail.com>
2022-08-25 23:51:45 +0200

    s

--- math/more_advanced_calc.py
+++ math/more_advanced_calc.py
@@ -1,3 +1,18 @@
 #this is a calc which can do multible operations
 #this bot uses python 3
 print("Welcome to the calculator")
+
+#create the multible function for the different operations
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+print("Select operation.")
