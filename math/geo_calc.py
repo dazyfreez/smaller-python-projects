86bee9e49b0542696f648c031c4ab7a7d379dc40
Christian Gragert <dazyfreez@gmail.com>
2022-09-16 20:32:05 +0200

    s

--- math/geo_calc.py
+++ math/geo_calc.py
@@ -1,7 +1,9 @@
 import math
+from secrets import choice
 
-print("Welcome to the calculator")
+print("Welcome to the geo_calculator")
 
+#create a list of all the functions
 def circle_area(radius):
     return math.pi * radius**2
 def circle_circumference(radius):
@@ -28,3 +30,33 @@ def triangle_base(height, area):
     return (2 * area) / height
 def triangle_side1(side2, side3, perimeter):
     return perimeter - side2 - side3
+
+# create a list of all the functions to choose from
+print("Select operation.")
+print("do you want to calculate something with a circle? type 1")   
+print("do you want to calculate something with a square? type 2")   
+print("do you want to calculate something with a triangle? type 3")
+# create a variable for the user input
+choice = input("Enter choice(1/2/3):")
+
+if choice in ('1','2','3'):
+    if choice =="1":
+        print("do you want to calculate the area of a circle? type 1")
+        print("do you want to calculate the circumference of a circle? type 2")
+        print("do you want to calculate the diameter of a circle? type 3")
+        print("do you want to calculate the radius of a circle? type 4")
+        choice = input("Enter choice(1/2/3/4):")
+        if choice == "1":
+            radius = int(input("Enter the radius of the circle: "))
+            print("The area of the circle is", circle_area(radius))
+        elif choice == "2":
+            radius = int(input("Enter the radius of the circle: "))
+            print("The circumference of the circle is", circle_circumference(radius))
+        elif choice == "3":
+            radius = int(input("Enter the radius of the circle: "))
+            print("The diameter of the circle is", circle_diameter(radius))
+        elif choice == "4":
+            diameter = int(input("Enter the diameter of the circle: "))
+            print("The radius of the circle is", circle_radius(diameter))
+        else:
+            print("Invalid input")
