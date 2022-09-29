71537ed8f6b265e11d3b84cb8d7a300cb2f6d633
Christian Gragert <dazyfreez@gmail.com>
2022-09-29 18:17:46 +0200

    s

--- math/even_better_calc.py
+++ math/even_better_calc.py
@@ -7,14 +7,7 @@ def mitternachtsformel (a,b,c):
     x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
     return x1, x2
 def discriminant(a,b,c):
-    print (b**2 - 4*a*c)
-    if discriminant(a,b,c) > 0:
-        print("The equation has two solutions")
-    elif discriminant(a,b,c) == 0:
-        print("The equation has one solution")
-    else:
-        print("The equation has no solution")
-
+    return b**2 - 4*a*c
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
