d9c7d05a5ea55bd0dbc63e59f9060eb38a5356d0
Christian Gragert <dazyfreez@gmail.com>
2022-10-02 19:43:51 +0200

    s

--- i/i.py
+++ i/i.py
@@ -3,11 +3,12 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-def sinus():
+def sinus_cosinus():
     x = np.arange(0, 4* np.pi, 0.1)
     y = np.sin(x)
     z = np.cos(x)
     plt.plot(x,y,x,z)
     plt.show()
 sinus()
+def 
 
