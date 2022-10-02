2c125f39842d673e15fa56e734a57ec481f6ef92
Christian Gragert <dazyfreez@gmail.com>
2022-10-02 19:44:40 +0200

    s

--- i/i.py
+++ i/i.py
@@ -10,5 +10,10 @@ def sinus_cosinus():
     plt.plot(x,y,x,z)
     plt.show()
 sinus_cosinus()
-def 
+def show_function():
+    x = np.arange(-10, 10, 0.1)
+    y = 2*x**2 + 3*x + 1
+    plt.plot(x,y)
+    plt.show()
+show_function()
 
