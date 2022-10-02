1ad19734ee511a0f8aec56df388394459d75d406
Christian Gragert <dazyfreez@gmail.com>
2022-10-02 19:46:04 +0200

    s

--- i/i.py
+++ i/i.py
@@ -17,3 +17,15 @@ def show_function():
     plt.show()
 show_function()
 
+def main():
+    print("1. Sinus and cosinus")
+    print("2. Show function")
+    ex = input("Enter the number of the exercise you want to run: ")
+    if ex == "1":
+        sinus_cosinus()
+    elif ex == "2":
+        show_function()
+    else:
+        print("Invalid input")
+        sys.exit()
+
